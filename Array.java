3b59de8ab135ed4d0f0fd5c400829069e74b1013
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-02-28 19:13:01 +0530

    Add Array.java with main method and sample arrays

---
+++ Array.java
@@ -0,0 +1,17 @@
+ // array
+
+import java.util.Scanner;
+ public class array {
+      public static void main(String[] args) {
+
+            int[] marks = { 10, 20, 30, 40, 50 };
+            int[][] finalMarks = {{10, 20, 30, 40, 50}, {60, 70, 80, 90, 100}};
+        
+            System.out.println(finalMarks[1][3]);
+            System.out.println(finalMarks[1][4]);
+    }
+}
+    
+    
+
+
