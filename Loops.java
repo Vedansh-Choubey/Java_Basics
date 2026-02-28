2834fec5e10931abe00615691660877e78149fbd
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-02-28 19:46:03 +0530

    Refactor Loops class
    Refactor Loops class and Testing For loop

---
+++ Loops.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // for Loop
+        
+        for(int i = 100; i>0; i--){
+            System.out.println(i);
+        }  
+    }
+    }
+
+
