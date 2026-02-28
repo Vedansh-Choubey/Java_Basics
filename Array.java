0f380cd48541771da9f6dced520db45313e7ee8b
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-02-28 19:27:57 +0530

    Refactor Array.java to include string array example
    Uncommented code for array initialization and printing elements. Added string array and print statements.

--- Array.java
+++ Array.java
@@ -5,22 +5,35 @@ import java.util.Scanner;
       public static void main(String[] args) {
 
 //            int[] marks = { 10, 20, 30, 40, 50 };
-//            int[][] finalMarks = {{10, 20, 30, 40, 50}, {60, 70, 80, 90, 100}};
-        
+//            int[][] finalMarks = {{10, 20, 30, 40, 50}, {60, 70, 80, 90, 100}};        
 //            System.out.println(finalMarks[1][3]);
 //            System.out.println(finalMarks[1][4]);
   
-        int[] arr = new int[5];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
+//        int[] arr = new int[5];
+//        arr[0] = 1;
+//        arr[1] = 2;
+//        arr[2] = 3;
+//        arr[3] = 4;
+//        arr[4] = 5;
+//        for (int i = 0; i < arr.length; i++) {
+//              System.out.println(arr[i]);
+//    }
+      //array of strings
+       String[] names = new String[5];
+       names[0] = "john";
+       names[1] = "doe";
+       names[2] = "jane";
+       names[3] = "smith";
+       names[4] = "johnson";
 
-        for (int i = 0; i < arr.length; i++) {
-              System.out.println(arr[i]);
-    }
-
+   //Printing every Element 
+       for (int i = 0; i < names.length; i++) {
+             System.out.println(names[i]);
+               }
+    //Printing Perticuler String of an index
+       System.out.println(names[1]);
+       System.out.println(names[4]);
+ 
        
       }   
       }
