5a32fb1cba200f7eb51ad4155b77c79642fbeb2f
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-02-28 19:47:07 +0530

    Modify Loops.java to use a while loop
    Commented out the for loop and added a while loop that increments and prints a user-provided number until it reaches 100.

--- Loops.java
+++ Loops.java
@@ -4,9 +4,19 @@ public class Loops {
     public static void main(String[] args) {
         // for Loop
         
-        for(int i = 100; i>0; i--){
+//        for(int i = 100; i>0; i--){
+//            System.out.println(i);
+//        }  
+ // while Loop
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int i = sc.nextInt();
+        while(i<100){
+            i++;
             System.out.println(i);
-        }  
+        }
+    
     }
     }
 
