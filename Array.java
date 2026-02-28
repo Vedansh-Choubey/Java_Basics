52c835d4645da94e5c3963b0fb4b6657884dcf8f
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-02-28 19:18:50 +0530

    Refactor array initialization and add print loop
    Commented out the original marks and finalMarks arrays and their print statements. Added a new array initialization and print loop for the new array.

--- Array.java
+++ Array.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
  public class array {
       public static void main(String[] args) {
 
-            int[] marks = { 10, 20, 30, 40, 50 };
-            int[][] finalMarks = {{10, 20, 30, 40, 50}, {60, 70, 80, 90, 100}};
+//            int[] marks = { 10, 20, 30, 40, 50 };
+//            int[][] finalMarks = {{10, 20, 30, 40, 50}, {60, 70, 80, 90, 100}};
         
-            System.out.println(finalMarks[1][3]);
-            System.out.println(finalMarks[1][4]);
+//            System.out.println(finalMarks[1][3]);
+//            System.out.println(finalMarks[1][4]);
+  
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        for (int i = 0; i < arr.length; i++) {
+              System.out.println(arr[i]);
     }
-}
+
+       
+      }   
+      }
+
     
     
 
