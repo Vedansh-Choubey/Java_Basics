4e609bde24d73ddc734811d831ba3571defa9149
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-01 23:03:47 +0530

    Create Casting.java
    Removed explicit casting example from Casting.java

---
+++ Casting.java
@@ -0,0 +1,11 @@
+public class casting {
+    public static void main(String[] args) {
+
+        //casting
+        // implicit casting
+
+        double price = 100;
+        double finalPcice = price + 18.8;
+        System.out.println(finalPcice);
+    }
+}
