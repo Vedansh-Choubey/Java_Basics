668d8c150b6029aeb3b65848b5bc5c9d4f5b6af6
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-01 23:07:02 +0530

    Aded explicit casting examples in Casting.java
    Commented out implicit casting example and added explicit casting example.

--- Casting.java
+++ Casting.java
@@ -4,8 +4,15 @@ public class casting {
         //casting
         // implicit casting
 
-        double price = 100;
-        double finalPcice = price + 18.8;
-        System.out.println(finalPcice);
+//        double price = 100;
+//        double finalPcice = price + 18.8;
+//        System.out.println(finalPcice);
+
+        // explicit casting
+        
+        int p = 100;
+        int fP = p + (int)18.4;
+        System.out.println(fP);
+
     }
 }
