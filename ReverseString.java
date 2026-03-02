16164d5ff2a2b5e8fa995a89740cdcaf9023440f
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-02 20:59:49 +0530

    Implement main method for string reversal
    Added main method to reverse input string from user.

--- ReverseString.java
+++ ReverseString.java
@@ -5,7 +5,15 @@ public class ReverseString {
     public static String reverse(String str) {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString();
-    }
+   }
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        System.out.println("Reversed String: " + reverse(input));
+
+        sc.close();
+  }
 }
