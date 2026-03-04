f928786ab8893b2c9846137c903b5e8a95b7dd70
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-04 10:15:05 +0530

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,38 @@
+public class operator {
+    public static void main(String[] args) {
+        // operator
+        // arithmetic operator
+        
+        // int a = 10;
+        // int b = 4;
+        // System.out.println(a + b);
+        // System.out.println(a - b);
+        // System.out.println(a * b);
+        // System.out.println(a / b);
+        // System.out.println(a % b);
+
+        // assignment operator
+
+        // unary operator
+
+        // int num  = 5;
+        // System.out.println(num++);
+        // System.out.println(++num);
+        // System.out.println(num--);
+        // System.out.println(--num);
+
+        //comparison operator
+        // a == b;
+        // a != b;
+        // a > b;
+        // a < b;
+        // a >= b;
+        // a <= b;
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+        // System.out.println(a >= b);
+        }    
+    }
+ 
