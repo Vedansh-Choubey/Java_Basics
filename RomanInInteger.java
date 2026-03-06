f8dbbdb2ea697d7b47b9f78c476c60c5797c9899
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-06 19:58:16 +0530

    Add Roman numeral to integer conversion
    Implement a method to convert Roman numerals to integers.

---
+++ RomanInInteger.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class RomanInInteger {
+
+    public static int romanToInt(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1)
+        map.put('V', 5)
+        map.put('X', 10)
+        map.put('L', 50)
+        map.put('C', 100)
+        map.put('D', 500)
+        map.put('M', 1000);
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int current = map.get(s.charAt(i));
+
+            if (i < s.length() - 1 &&
+                current < map.get(s.charAt(i + 1))){
+                total -= current;   // subtraction case
+            } else {
+                total += current;   // addition
+            }
+    
+
+        return total;
+    }
+
+public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Roman numeral: ");
+        String roman = sc.nextLine().toUpperCase();
+
+        int result = romanToInt(roman);
+
+        System.out.println("Integer value: " + result);
+
+        sc.close();
+    }
+}
