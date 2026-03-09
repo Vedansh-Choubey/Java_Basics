40ad32f9f3332164ff3c955506804787989fcdb2
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-09 21:06:24 +0530

    Create AreaOfTriangle.java

---
+++ AreaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter base of triangle: ");
+            double base = scanner.nextDouble();
+
+            System.out.print("Enter height of triangle: ");
+            double height = scanner.nextDouble();
+
+            double area = 0.5 * base * height;
+
+            System.out.println("Area of triangle: " area);
+        }
+    }
+}
