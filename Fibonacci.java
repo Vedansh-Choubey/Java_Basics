6542b5e059d96fc14afc207073a4eb59102fb8a8
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-14 22:24:41 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void printFibonacci(int n) {
+        long a = 0, b = 1;
+
+        System.out.print("Fibonacci series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            long next = a + b;
+            a = b;
+            b =long next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        printFibonacci(n);
+
+        sc.close();
+    }
+}
