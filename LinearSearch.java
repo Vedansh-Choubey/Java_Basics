84a962d63206ccfd1154c013d94a77500e003aac
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-16 21:19:44 +0530

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        int target = sc.nextInt();
+
+        int index = linearSearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element is found at index: " + index);
+         else {
+            System.out.println("Element is not found.");
+        
+
+        sc.close();
+    }
+}
