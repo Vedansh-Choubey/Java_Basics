08923db8097fe6e2e1c9f98194ecf0abe97efbbb
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-18 20:52:41 +0530

    Update NumberSwapping.java

--- NumberSwapping.java
+++ NumberSwapping.java
@@ -9,14 +9,20 @@ public class NumberSwapping {
             System.out.print("Enter second number: ");
             int num2 = scanner.nextInt();
 
-            System.out.println("\nBefore swapping:");
-            System.out.println("First number: " + num1);
-            System.out.println("Second number: " + num2);
+            System.out.println("Before swapping The Numbers:");
+            System.out.println("1st Number: " + num1);
+            System.out.println("2nd Number: " + num2);
 
-            num1 = num1 + num2;
-            num2 = num1 - num2;
-            num1 = num1 - num2;
+              // Swap without using third variable
+           // num1 = num1 + num2;
+           // num2 = num1 - num2;
+           // num1 = num1 - num2;
 
+            // Swap using Temp variable
+            
+             int temp = num1;
+             num1 = num2;
+             num2 = temp; 
             System.out.println("After swapping The Numbers:");
             System.out.println("1st Number: " + num1);
             System.out.println("2nd Number: " + num2);
