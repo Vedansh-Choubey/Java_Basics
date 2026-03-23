b79d1d719183f58838511772125903242b8cd44c
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-23 23:17:48 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter principal amount: ");
+            double principal = scanner.nextDouble();
+
+            System.out.print("Enter rate of interest: ");
+            double rate = scanner.nextDouble();
+
+            System.out.print("Enter time period (in years): ");
+            double time = scanner.nextDouble();
+
+            double simpleInterest = (principal * rate * time) / 100;
+
+            System.out.print("Simple Interest: " + simpleInterest);
+            System.out.print("Total Amount: " + (principal + simpleInterest));
+        }
+    }
+}
