e7701244faf208c410b553f1ef55fe3c9273bd7f
Vedansh choubey <vedanshchoubeyvedu2006@gmail.com>
2026-03-27 23:50:24 +0530

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -7,7 +7,7 @@ public class CheckPerfectNumber {
         int number = scanner.nextInt();
 
         int sum = 0;
-        for (int i = 1; i < number / 2; i++) {
+        for (int i = 1; i <= number / 2; i++) {
             if (number % i == 0) {
                 sum += i;
             }
