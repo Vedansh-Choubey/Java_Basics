6c390f0cbedb3c90bb991aae2ae577385ea306fe
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-05 20:51:30 +0530

    Correct loop in removeDuplicates method
    Fix loop condition to prevent index out of bounds error.

---
+++ RemoveDuplicates.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length() -1; i++) {
+            char ch = str.charAt(i)
+
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        System.out.println("After removing duplicates: " + removeDuplicates(str));
+
+        sc.close();
+    }
+}
