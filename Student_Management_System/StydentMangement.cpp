d6894643d098929e9eef0f27bc8368453c1f2de7
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-17 23:25:20 +0530

    Correct syntax errors and enhance student search
    Fixed syntax errors and improved student search functionality.

--- Student_Management_System/StydentMangement.cpp
+++ Student_Management_System/StydentMangement.cpp
@@ -27,7 +27,8 @@ public class StudentManagement {
         Scanner sc = new Scanner(System.in);
         Student[] students = new Student[5];
 
-        int count = 0:
+        int count = 0;
+        int choice;
 
         do {
             System.out.println("\n1. Add Student");
@@ -36,5 +37,46 @@ public class StudentManagement {
             System.out.println("4. Exit");
             System.out.print("Enter choice: ");
             choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    if (count < 5) {
+                        students[count] = new Student();
+                        students[count].input();
+                        count++;
+                    } else {
+                        System.out.println("Storage Full!");
+                    }
+                    break;
 
+                case 2:
+                    for (int i = 0; i < count; i++) {
+                        students[i].display();
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID to search: ");
+                    int searchId = sc.nextInt();
+                    boolean found = false;
+
+                    for (int i = 0; i < count; i++) {
+                        if (students[i].id == searchid) {
+                            students[i].display();
+                            found = true;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Student not found!");
+                    }
+                    break;
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
+
+  
             
